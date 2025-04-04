4e70b9bd5fc667d628ddb5787a55678136737b73
Alex <alex.guiu@insbaixcamp.cat>
2025-04-04 14:49:10 +0200

    Més codi

--- main.java
+++ main.java
@@ -3,6 +3,14 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Afegim una segona linia");
+        System.out.println("Afegim una tercera linia");
+        System.out.println("Afegim una linia desde el visual");
+        System.out.println("Afegim una linia desde el github");
+        
+        if (3 < 4) {
+            System.out.println(" Entrem dintre de l'if");
+            
+        }
         
     }
 }
